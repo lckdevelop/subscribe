aaca85020bc94128699418b339d36471303ba961
chaekyeong <chaekyeong2@gmail.com>
2021-07-12 16:04:47 +0900

    찜 효과

--- src/main/java/dev/department/subscribe/sec/ErrorController.java
+++ src/main/java/dev/department/subscribe/sec/ErrorController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ErrorController {
+	@GetMapping("/error400")
+	public String error400() {
+		
+		return "errorpage/error400";
+	}
 	
 	@GetMapping("/error403")
 	public String error403() {
